051338403dfddbb9a68615df976ae0596dac16e3
Andy Phan <andyphan@pop-os.localdomain>
2023-02-25 16:12:35 +1030

    Created a module to generate the footer section

---
+++ src/footer.js
@@ -0,0 +1,95 @@
+// Generate the footer HTML
+function footer() {
+    const footer = document.createElement('footer');
+    const container = document.createElement('div');
+    container.classList.add('footer-info', 'container');
+
+    const row = document.createElement('div');
+    row.classList.add('row');
+
+    // Social
+    let col = document.createElement('div');
+    col.classList.add('col-sm', 'pb-3', 'container');
+    const social = document.createElement('div');
+    social.classList.add('row', 'social');
+    const social_title = document.createElement('h6');
+    social_title.innerText = 'Social:';
+    const social_icon = document.createElement('a');
+    social_icon.classList.add('social-icon');
+    social_icon.href = "https://www.facebook.com/GreenTeaHouseGlenelg";
+    social_icon.target = "_blank";
+    const social_img = document.createElement('img');
+    social_img.src = "../src/images/facebook.svg";
+    social_img.alt = "Facebook Icon";
+    social_icon.appendChild(social_img);
+    social.appendChild(social_title);
+    social.appendChild(social_icon);
+    col.appendChild(social);
+
+    // Contact
+    const contact = document.createElement('div');
+    contact.classList.add('row');
+    const contact_title = document.createElement('h6');
+    contact_title.innerText = "Contact:";
+    contact.appendChild(contact_title);
+    const phone = document.createElement('p');
+    phone.innerHTML = "(08) 8294 9484";
+    contact.appendChild(phone);
+    contact.classList.add('row');
+    col.appendChild(contact);
+    row.appendChild(col);
+
+    // Address
+    col = document.createElement('div');
+    col.classList.add('col-sm', 'pb-3');
+    
+    const address_obj = {"Address:": "h6", "22 Jetty Road": "p", "Glenelg SA 5045": "p"};
+    for(const addr in address_obj) {
+        let address = document.createElement(address_obj[addr]);
+        address.innerText = addr;
+        col.appendChild(address);
+    }
+    row.appendChild(col);
+
+    // Business hours
+    col = document.createElement('div');
+    col.classList.add('col-sm', 'pb-3');
+    let hours = document.createElement('h6');
+    hours.innerText = "Business Hours:";
+    col.appendChild(hours);
+    
+    hours = {"Monday:": "Closed", "Tues - Sun:": "12:00 - 3:00 PM", "": "5:00 - 8:00 PM"};
+    for(const time in hours) {
+        let business_row = document.createElement('div');
+        business_row.classList.add('row')
+        let business_col = document.createElement('div');
+        business_col.className = "col";
+        business_col.innerText = time;
+        business_row.appendChild(business_col);
+        business_col = document.createElement('div');
+        business_col.className = "col";
+        business_col.innerText = hours[time];
+        business_row.appendChild(business_col);
+        col.appendChild(business_row);
+    }
+    row.appendChild(col);
+
+    // Developer Credit
+    const developed_by = document.createElement('p');
+    developed_by.classList.add('text-center');
+    developed_by.innerText = "Developed by Andy Phan ";
+    const github = document.createElement('a');
+    github.href = "https://github.com/xapzx";
+    const img = document.createElement('img');
+    img.src = "../src/images/github.svg";
+    img.alt = "Github Icon";
+    github.appendChild(img);
+    developed_by.appendChild(github);
+
+    container.appendChild(row);
+    footer.appendChild(container);
+    footer.appendChild(developed_by);
+    return footer; 
+}
+
+export default footer;
--- src/index.js
+++ src/index.js
@@ -1,82 +1,14 @@
 // Import our custom CSS
-import './style.scss'
+import './style.scss';
 
 // Import all of Bootstrap's JS
-import * as bootstrap from 'bootstrap'
+import * as bootstrap from 'bootstrap';
 
-// Create Home page
-function component() {
-   const element = document.createElement('main');
-   const nav = navbar();
-   element.appendChild(nav);
+// Import modules
+import home from "./home.js"
+import footer from "./footer.js";
 
-   return element;
-}
+const content = document.querySelector('.content');
 
-// Create nav bar
-function navbar() {
-    const nav = document.createElement('nav');
-    nav.classList.add('navbar', 'navbar-expand-lg');
-
-    const container = document.createElement('div');
-    container.classList.add('container-fluid');
-
-    const logo = document.createElement('a');
-    logo.classList.add('navbar-brand');
-    logo.href = '#';
-    logo.innerText = "GREEN TEA HOUSE";
-
-    const btn = document.createElement('button');
-    btn.classList.add('navbar-toggler', 'navbar-dark');
-    btn.dataset.bsToggle = "collapse";
-    btn.dataset.bsTarget = "#navbarNav"
-    btn.type="button";
-    btn.setAttribute("aria-controls", "navbarNav");
-    btn.setAttribute("aria-expanded", "false");
-    btn.setAttribute("aria-label", "Toggle navigation");
-
-    const burger = document.createElement('span');
-    burger.classList.add('navbar-toggler-icon');
-    btn.appendChild(burger);
-
-    const navbar = document.createElement('div');
-    navbar.classList.add('collapse', 'navbar-collapse');
-    navbar.setAttribute('id', 'navbarNav');
-    navbar.appendChild(navItem(['HOME', 'MENU', 'CONTACT']));
-
-    container.appendChild(logo);
-    container.appendChild(btn);
-    container.appendChild(navbar);
-    nav.appendChild(container);
-    return nav;
-}
-
-// Create nav items
-// Parameter: Array of strings
-function navItem(items) {
-    let flag = 0;
-    const ul = document.createElement('ul');
-    ul.classList.add('navbar-nav');
-
-    for(const item of items) {
-        let nav_item = document.createElement('li');
-        nav_item.classList.add('nav-item');
-        let nav_link = document.createElement('a');
-        nav_link.classList.add('nav-link');
-
-        if(!flag) {
-            nav_link.classList.add('active');
-            nav_link.setAttribute('aria-current', 'page');
-        }
-        
-        nav_link.href = "#";
-        nav_link.innerText = item;
-        nav_item.appendChild(nav_link);
-        ul.appendChild(nav_item);
-
-        flag++;
-    }
-    return ul;
-}
-
-document.querySelector('.content').appendChild(component());
+content.appendChild(home());
+content.appendChild(footer());
