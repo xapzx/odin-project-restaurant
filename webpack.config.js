34bf5a6d0051745934e64cb72aed81d085f03915
Andy Phan <andyphan@live.com.au>
2023-04-11 22:37:11 +0930

    Fixed issue with inline images not showing

--- webpack.config.js
+++ webpack.config.js
@@ -8,6 +8,7 @@ module.exports = {
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
+    assetModuleFilename: 'images/[name]-[hash][ext][query]',
     clean: true,
   },
   module: {
@@ -47,6 +48,6 @@ module.exports = {
       template: 'src/index.html',
       filename: 'index.html',
       inject: false,
-    })
+    }),
   ],
 };
